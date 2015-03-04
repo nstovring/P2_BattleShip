0ad6ee33079e044a977b29a89e9519f97fea7855
nstovring <nstovring@live.dk>
2015-03-04 14:37:29 +0100

    Project files

---
+++ P2 Networking/Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	private float lastSynchonizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+
+	public float speed = 10f;
+	
+	void Update()
+	{
+		if(GetComponent<NetworkView>().isMine){
+		InputMovement();
+		}
+	}
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info){
+		Vector3 syncPosition = Vector3.zero;
+
+		if(stream.isWriting){
+			syncPosition = GetComponent<Rigidbody>().position;
+			stream.Serialize(ref syncPosition);
+		}else{
+			stream.Serialize(ref syncPosition);
+
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchonizationTime; 
+			lastSynchonizationTime = Time.time;
+
+			syncStartPosition = GetComponent<Rigidbody>().position;
+			syncEndPosition = syncPosition;
+			//rigidbody.position = syncPosition;
+		}
+	}
+	
+	void InputMovement()
+	{
+		if(Input.GetAxis("Horizontal") > 0.1 ||Input.GetAxis("Horizontal") < 0.1 || Input.GetAxis("Vertical") > 0.1|| Input.GetAxis("Vertical") < 0.1){
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"))* speed * Time.deltaTime);
+		}
+	}
+}
---
+++ P2 Networking/Assets/_scripts/NetworkManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "P2 Networking";
+
+	public GameObject playerPrefab;
+
+	private HostData[] hostList;
+
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	// Use this for initialization
+	void OnServerInitialized()
+	{
+		SpawnPlayer();
+		Debug.Log("Server Initializied");
+	}
+	void OnConnectedToServer()
+	{
+		SpawnPlayer();
+		Debug.Log("Server Joined");
+	}
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+	}
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
